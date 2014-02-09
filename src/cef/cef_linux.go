8caa0d09b0bc0324b9079e3872a7cb24b3f30f74
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-02-09 01:50:36 +0100

    On Linux there is no appHandle in mainArgs. Instead os.Args needs to be converted to C equivalent argc/argv. Lots of work to copy an array of strings, and it doesn't look too safe, using malloc and lots of pointers. Let us hope that it won't explode.

--- src/cef/cef_linux.go
+++ src/cef/cef_linux.go
@@ -12,3 +12,26 @@ package cef
 #include "include/capi/cef_app_capi.h"
 */
 import "C"
+import "unsafe"
+import (
+    "os"
+)
+
+func FillMainArgs(mainArgs *C.struct__cef_main_args_t,
+        appHandle unsafe.Pointer) {
+    // On Linux appHandle is nil.
+    // Converting os.Args to C equivalent argc/argv.
+    var char *C.char
+    charSize := int(unsafe.Sizeof(char))
+    argv := C.malloc(C.size_t(charSize * len(os.Args)))
+    for i, arg := range os.Args {
+        // These C strings are not supposed to be freed,
+        // do not call C.free().
+        charArg := C.CString(arg)
+        ptr := unsafe.Pointer(uintptr(argv) + uintptr(charSize * i))
+        ptrSize := C.size_t(unsafe.Sizeof(ptr))
+        C.memcpy(ptr, unsafe.Pointer(charArg), ptrSize)
+    }
+    mainArgs.argc = C.int(len(os.Args))
+    mainArgs.argv = (**C.char)(argv)
+}

d6afe9ecb2484aff72475ea2926ba8a5ccbdcbe0
Czarek Tomczak <czarek.tomczak@gmail.com>
2014-02-24 23:52:11 +0100

    Added "noconsole" parameter to build.bat script on Windows. When specified the executable won't display console window.

--- build.bat
+++ build.bat
@@ -31,8 +31,13 @@ go test src/tests/cef_test.go
     @del "%~dp0Release\cef2go.exe"
 )
 
-go build -o Release/cef2go.exe src/main_windows.go
-@if %ERRORLEVEL% neq 0 goto end
+IF "%1"=="noconsole" (
+    go build -ldflags="-H windowsgui" -o Release/cef2go.exe src/main_windows.go
+    @if %ERRORLEVEL% neq 0 goto end
+) else (
+    go build -o Release/cef2go.exe src/main_windows.go
+    @if %ERRORLEVEL% neq 0 goto end
+)
 
 cd Release/
 call "cef2go.exe"
